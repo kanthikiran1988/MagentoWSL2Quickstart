6cd2a59ee357d42cc2cb644a639a3b465ff135af
kanthikiran1988 <51862710+kanthikiran1988@users.noreply.github.com>
2024-04-29 12:03:46 +0530

    Updating WSL Installation

--- install_wsl.ps1
+++ install_wsl.ps1
@@ -9,23 +9,9 @@ if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdenti
 Write-Output "Enabling WSL..."
 dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
-
-# Detect processor architecture and install the correct WSL update
-$architecture = (Get-WmiObject -Class Win32_Processor).AddressWidth
-$wslUpdateUrl = "https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_"
-if ($architecture -eq 64) {
-    $wslUpdateUrl += "x64.msi"
-} elseif ($architecture -eq 32) {
-    Write-Output "32-bit systems are not supported for WSL 2."
-    exit
-} else {
-    $wslUpdateUrl += "arm64.msi"
-}
-
 Write-Output "Downloading WSL update package for your architecture..."
-Invoke-WebRequest -Uri $wslUpdateUrl -OutFile "wsl_update.msi" -UseBasicParsing
-Write-Output "Installing WSL update..."
-Start-Process msiexec.exe -ArgumentList '/i', 'wsl_update.msi', '/quiet', '/norestart' -Wait
+wsl --update
+Write-Output "Installing WSL Ubuntu..."
 wsl --install -d Ubuntu
 
 # Ask the user if they want to restart now
